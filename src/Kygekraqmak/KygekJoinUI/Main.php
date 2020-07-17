d29d093d40a6953f58494643fd42523c5126d4cd
Kygekraqmak <68368066+Kygekraqmak@users.noreply.github.com>
2020-07-17 09:36:36 +0800

    Update and rename src/Kygekraqmak/Main.php to src/Kygekraqmak/KygekJoinUI/Main.php

--- src/Kygekraqmak/KygekJoinUI/Main.php
+++ src/Kygekraqmak/KygekJoinUI/Main.php
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-namespace Kygekraqmak\KygekJoinUI\Main;
+namespace Kygekraqmak\KygekJoinUI;
 
 use pocketmine\event\player\PlayerJoinEvent;
 
